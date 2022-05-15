0b871c3fb352c91effed19c38a965d90b96baf88
tolga <tolgayyildirim87@gmail.com>
2022-05-15 11:05:14 -0500

    the pink car is ready with basic features

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,12 @@ public class Main {
     public static void main(String[] args) {
 
         BasicFeatures car = new BasicFeatures();
-        
+        car.color="pink";
+        car.regNo="BMW";
+        car.headLights="red";
+        car.engineCapacity="V12";
+        car.doors="7";
+
+
     }
 }
